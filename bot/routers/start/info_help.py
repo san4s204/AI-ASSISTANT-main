996449f68e74fcc0210203e80368fa020ebdc0d6
san4s204 <sasha.kosenkov20@mail.ru>
2025-12-14 21:39:42 +1000

    fix parse_mode in info and help messages

--- bot/routers/start/info_help.py
+++ bot/routers/start/info_help.py
@@ -12,19 +12,18 @@ async def help_cmd(message: types.Message):
 
 @router.message(Command("info"))
 async def info_cmd(message: types.Message):
-    await message.answer(info_text(), reply_markup=keyboard_return())
+    await message.answer(info_text(), reply_markup=keyboard_return(), parse_mode="HTML")
 
 @router.message(F.text == "📖 Информация")
 async def info_reply(message: types.Message):
-    await message.answer(info_text(), reply_markup=keyboard_return())
-
+    await message.answer(info_text(), reply_markup=keyboard_return(), parse_mode="HTML")
 @router.message(F.text == "💬 Помощь")
 async def help_reply(message: types.Message):
     await message.answer("Если у вас появятся вопросы, всегда обращайтесь к @chess_it_manager", reply_markup=keyboard_return())
 
 @router.callback_query(F.data == "info")
 async def cq_info(callback: types.CallbackQuery):
-    await callback.message.edit_text(info_text(), reply_markup=keyboard_return())
+    await callback.message.edit_text(info_text(), reply_markup=keyboard_return(), parse_mode="HTML")
 
 @router.callback_query(F.data == "help")
 async def cq_help(callback: types.CallbackQuery):
